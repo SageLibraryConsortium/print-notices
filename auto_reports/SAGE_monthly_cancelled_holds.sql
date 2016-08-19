a2d6a80ad1f50585c40f5685bf1058795e5e2bfe
brent <brent@hoodriverlibrary.org>
2016-08-19 13:39:20 -0700

    Update SAGE_monthly_cancelled_holds.sql

--- auto_reports/SAGE_monthly_cancelled_holds.sql
+++ auto_reports/SAGE_monthly_cancelled_holds.sql
@@ -1,5 +1,7 @@
 -- Monthly Holds Cancelled Report --
-
+\o /home/opensrf/scripts/auto_reports/SAGE_monthly_cancelled_holds2.csv
+\a
+\f |
 SELECT * FROM (SELECT	EXTRACT(YEAR FROM "3b0d7d05c65006de7b2ca20f5fe39690"."cancel_time") || '-' || LPAD(EXTRACT(MONTH FROM "3b0d7d05c65006de7b2ca20f5fe39690"."cancel_time")::text,2,'0') AS "Hold Cancel Date",
 	"743560702872e3e6d9c272eaa7e36b74"."shortname" AS "Patron's Home Library",
 	"4f6ab23c031294937877e3e686649b0e"."shortname" AS "Pickup Library",
@@ -10,7 +12,9 @@ SELECT * FROM (SELECT	EXTRACT(YEAR FROM "3b0d7d05c65006de7b2ca20f5fe39690"."canc
 	INNER JOIN actor.usr AS "b7ef782e491021c8b18982b09cede6dd" ON ("3b0d7d05c65006de7b2ca20f5fe39690"."usr" = "b7ef782e491021c8b18982b09cede6dd"."id")
 	INNER JOIN actor.org_unit AS "743560702872e3e6d9c272eaa7e36b74" ON ("b7ef782e491021c8b18982b09cede6dd"."home_ou" = "743560702872e3e6d9c272eaa7e36b74"."id")
 	INNER JOIN actor.org_unit AS "4f6ab23c031294937877e3e686649b0e" ON ("3b0d7d05c65006de7b2ca20f5fe39690"."pickup_lib" = "4f6ab23c031294937877e3e686649b0e"."id")
-  WHERE	EXTRACT(YEAR FROM "3b0d7d05c65006de7b2ca20f5fe39690"."cancel_time") || '-' || LPAD(EXTRACT(MONTH FROM "3b0d7d05c65006de7b2ca20f5fe39690"."cancel_time")::text,2,'0') BETWEEN EXTRACT(YEAR FROM $_10618$2016-08-19 13:29:06.686237-07$_10618$::TIMESTAMPTZ + $_10618$-1 months$_10618$) || $_10618$-$_10618$ || LPAD(EXTRACT(MONTH FROM $_10618$2016-08-19 13:29:06.686237-07$_10618$::TIMESTAMPTZ + $_10618$-1 months$_10618$)::text,2,$_10618$0$_10618$) AND EXTRACT(YEAR FROM $_10618$2016-08-19 13:29:06.686237-07$_10618$::TIMESTAMPTZ + $_10618$-1 months$_10618$) || $_10618$-$_10618$ || LPAD(EXTRACT(MONTH FROM $_10618$2016-08-19 13:29:06.686237-07$_10618$::TIMESTAMPTZ + $_10618$-1 months$_10618$)::text,2,$_10618$0$_10618$)
+  WHERE date_trunc('MONTH'::text, "3b0d7d05c65006de7b2ca20f5fe39690"."cancel_time") = date_trunc('MONTH'::text, now() - '1 mon'::INTERVAL)
   GROUP BY 1, 2, 3, 4
   ORDER BY EXTRACT(YEAR FROM "3b0d7d05c65006de7b2ca20f5fe39690"."cancel_time") || '-' || LPAD(EXTRACT(MONTH FROM "3b0d7d05c65006de7b2ca20f5fe39690"."cancel_time")::text,2,'0') ASC, "743560702872e3e6d9c272eaa7e36b74"."shortname" ASC, "4f6ab23c031294937877e3e686649b0e"."shortname" ASC, "3aadd3567d41d9d82dacb9441a3f4076"."label" ASC, COUNT("3aadd3567d41d9d82dacb9441a3f4076"."id") ASC
-) limited_to_1048575_hits LIMIT 1048575;
+) limited_to_1048575_hits LIMIT 1048575
+;
+\o
